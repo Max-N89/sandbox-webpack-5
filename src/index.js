eb0d72a8829052958e5839ea71a8a010c4c1a613
Max-N89 <4work.maxn@gmail.com>
2021-03-25 17:12:06 +0200

    Covered topic - asset management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+import "./styles.css";
+import webpackLogo from "./assets/webpack-logo.svg";
+import exampleCSV from "./data-examples/example.csv";
+import exampleXML from "./data-examples/example.xml";
+import exampleTOML from "./data-examples/example.toml";
+import exampleYAML from "./data-examples/example.yaml";
+import exampleJSON5 from "./data-examples/example.json5";
 import _ from "lodash";
 
-function component() {
-    const element = document.createElement("h1");
+function createMainHeader() {
+    const header = document.createElement("header");
+    const logoImg = new Image();
+    const h1 = document.createElement("h1");
 
-    element.textContent = _.join(["Hello", "webpack."], " ");
+    logoImg.src = webpackLogo;
+    h1.textContent = _.join(["Hello", "from", "webpack"], " ");
 
-    return element;
+    header.append(logoImg, h1);
+
+    return header;
 }
 
-document.querySelector("main").append(component());
+const main = document.querySelector("#main");
+const mainHeader = createMainHeader();
+
+main.append(mainHeader);
+
+const dataExamples = new Map([
+    ["CSV", exampleCSV],
+    ["XML", exampleXML],
+    ["TOML", exampleTOML],
+    ["YAML", exampleYAML],
+    ["JSON5", exampleJSON5]
+]);
+
+dataExamples.forEach((value, key) => {
+    console.log(`>>> ${key} <<<`, value);
+});
